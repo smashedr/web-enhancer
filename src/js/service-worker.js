460c93c1bd447751aad1da5e9755420242cbd1d1
Shane <6071159+smashedr@users.noreply.github.com>
2025-10-30 11:51:05 -0700

    Add Sidepanel

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -9,6 +9,7 @@ import {
     showHidePassword,
     githubURL,
     purgeImageCache,
+    openSidePanel,
 } from './export.js'
 
 chrome.runtime.onStartup.addListener(onStartup)
@@ -54,6 +55,7 @@ async function onInstalled(details) {
             copy: true,
             password: true,
             purge: true,
+            sidePanel: true,
             options: true,
         },
         showUpdate: false,
@@ -98,8 +100,8 @@ async function onClicked(ctx, tab) {
         await activateOrOpen(url)
     } else if (ctx.menuItemId === 'showPanel') {
         await openExtPanel()
-        // } else if (ctx.menuItemId === 'openSidePanel') {
-        //     await openSidePanel() // TODO: add menu item
+    } else if (ctx.menuItemId === 'openSidePanel') {
+        await openSidePanel()
     } else if (ctx.menuItemId === 'copyText') {
         console.debug('injectFunction: copy')
         await injectFunction(copyActiveElementText, [ctx])
@@ -126,8 +128,8 @@ async function onCommand(command) {
         await activateOrOpen(url)
     } else if (command === 'showPanel') {
         await openExtPanel()
-        // } else if (command === 'openSidePanel') {
-        //     await openSidePanel() // TODO: add command
+    } else if (command === 'openSidePanel') {
+        await openSidePanel()
     }
 }
 
@@ -189,6 +191,10 @@ function createContextMenus(ctx) {
     }
     if (ctx.purge) {
         addContext([['image'], 'purgeImage', 'Purge Image Cache'])
+        collected.push('link')
+    }
+    if (ctx.sidePanel) {
+        addContext([['all'], 'openSidePanel', 'Side Panel'])
         collected.push('link')
     }
     if (ctx.options) {
--- src/js/sidepanel.js
+++ src/js/sidepanel.js
@@ -10,9 +10,22 @@ document
 document
     .querySelectorAll('.open-popup')
     .forEach((el) => el.addEventListener('click', openPopup))
+// document
+//     .querySelectorAll('.close-panel')
+//     .forEach((el) => el.addEventListener('click', closePanel))
 document
-    .querySelectorAll('.close-panel')
-    .forEach((el) => el.addEventListener('click', closePanel))
+    .querySelectorAll('[data-click]')
+    .forEach((el) => el.addEventListener('click', clickHandler))
+
+const inputForm = document.getElementById('input-form')
+const inputSelect = document.getElementById('input-select')
+inputSelect.addEventListener('change', selectChange)
+
+const selectInput = document.getElementById('select-input')
+selectInput.addEventListener('change', inputChange)
+// selectInput.addEventListener('click', inputClick)
+
+document.getElementById('select-input').addEventListener('change', inputChange)
 
 const hostnameEl = document.getElementById('hostname')
 
@@ -32,21 +45,100 @@ async function domContentLoaded() {
 }
 
 /**
- * Close Side Panel Click Callback
- * @function closePanel
- * @param {Event} [event]
+ * Click Handler Callback
+ * @param {UIEvent} [event]
  */
-async function closePanel(event) {
-    console.debug('closePanel:', event)
+async function clickHandler(event) {
+    // console.debug('clickHandler:', event)
+    const target = event.currentTarget
     event?.preventDefault()
-    // noinspection JSUnresolvedReference
-    if (typeof browser !== 'undefined') {
+    console.debug('target.dataset?.click:', target?.dataset?.click)
+    if (target?.dataset?.click === 'reload') {
+        selectInput.value = ''
+        inputForm.reset()
+        window.location.reload()
+    } else if (target?.dataset?.click === 'close') {
         // noinspection JSUnresolvedReference
-        await browser.sidebarAction.close()
-    } else {
-        window.close()
+        if (typeof browser !== 'undefined') {
+            // noinspection JSUnresolvedReference
+            await browser.sidebarAction.close()
+        } else {
+            window.close()
+        }
     }
 }
+
+/**
+ * Click Handler Callback
+ * @param {InputEvent} [event]
+ */
+async function selectChange(event) {
+    console.debug('selectChange:', event)
+    const target = event.currentTarget
+    event?.preventDefault()
+    console.debug('target:', target)
+    console.debug('target.value:', target.value)
+    const selectedOption = target.options[target.selectedIndex]
+    console.debug('selectedOption:', selectedOption)
+    selectInput.placeholder = selectedOption.textContent
+    selectInput.dataset.action = target.value
+}
+
+/**
+ * Input Change Callback
+ * @param {InputEvent} [event]
+ */
+async function inputChange(event) {
+    console.debug('inputChange:', event)
+    const target = event.currentTarget
+    event?.preventDefault()
+    console.debug('target:', target)
+    console.debug('target.value:', target.value)
+    console.debug('inputSelect.value:', inputSelect.value)
+    if (inputSelect.value === 'encode') {
+        console.debug('encode:', target.value)
+        const result = encodeURIComponent(target.value)
+        console.debug('result:', result)
+        await navigator.clipboard.writeText(result)
+    } else if (inputSelect.value === 'decode') {
+        console.debug('decode:', target.value)
+        const result = decodeURIComponent(target.value)
+        console.debug('result:', result)
+        await navigator.clipboard.writeText(result)
+    }
+
+    target.classList.add('is-valid')
+    setTimeout(() => event.target.classList.remove('is-valid'), 1000)
+}
+
+// /**
+//  * Input Click Callback
+//  * @param {InputEvent} [event]
+//  */
+// async function inputClick(event) {
+//     console.debug('inputClick:', event)
+//     const target = event.currentTarget
+//     // event?.preventDefault()
+//     console.debug('target:', target)
+//     console.debug('activeElement:', document.activeElement)
+// }
+
+// /**
+//  * Close Side Panel Click Callback
+//  * @function closePanel
+//  * @param {Event} [event]
+//  */
+// async function closePanel(event) {
+//     console.debug('closePanel:', event)
+//     event?.preventDefault()
+//     // noinspection JSUnresolvedReference
+//     if (typeof browser !== 'undefined') {
+//         // noinspection JSUnresolvedReference
+//         await browser.sidebarAction.close()
+//     } else {
+//         window.close()
+//     }
+// }
 
 /**
  * Tab Change Callback
